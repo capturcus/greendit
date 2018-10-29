6d333d766dfe07b719f1e65a7970f2184b8fc274
Marcin Parafiniuk <marcin.parafiniuk@gmail.com>
2018-10-29 08:54:47 +0100

    add auto token renew

--- src/app/interceptor.ts
+++ src/app/interceptor.ts
@@ -5,8 +5,8 @@ import {
   HttpEvent,
   HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError } from "rxjs/internal/operators";
+import { Observable, of, observable } from 'rxjs';
+import { catchError, switchMap } from "rxjs/internal/operators";
 import { RedditService } from './reddit.service';
 
 @Injectable()
@@ -17,13 +17,50 @@ export class RequestInterceptor implements HttpInterceptor {
   ) {
   }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  private renewAccessToken(): Observable<any> {
+    return Observable.create((observer) => {
+      let postLink = "https://www.reddit.com/api/v1/access_token";
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        var DONE = 4; // readyState 4 means the request is done.
+        var OK = 200; // status 200 is a successful return.
+        if (xhr.readyState === DONE) {
+          if (xhr.status === OK) {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+          }
+        }
+      }
+      xhr.open('POST', postLink);
+      xhr.setRequestHeader('Authorization', this.reddit.getBasicAuth());
+      xhr.setRequestHeader('Content-type', "application/x-www-form-urlencoded");
+      xhr.send("grant_type=refresh_token&refresh_token=" + localStorage.getItem("reddit_refresh_token"));
+    })
+  }
 
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((error, caught) => {
         //intercept the respons error and displace it to the console
         console.log(error);
-        this.handleError(error);
+        if (error.status === 401) {
+          if (error.url.includes("https://oauth.reddit.com")) {
+            console.log("handling error");
+            return this.renewAccessToken()
+              .pipe(
+                switchMap((data) => {
+                  console.log("INNER DATA", data);
+                  localStorage.setItem("reddit_access_token", data.access_token);
+                  let newRequest = request.clone({
+                    setHeaders: {
+                      Authorization: "bearer " + data.access_token
+                    }
+                  })
+                  return next.handle(newRequest)
+                })
+              )
+          }
+        }
         return of(error);
       }) as any);
   }
@@ -33,25 +70,7 @@ export class RequestInterceptor implements HttpInterceptor {
       if (err.url.includes("https://oauth.reddit.com")) {
 
         // a reddit api call failed, refresh.
-        let postLink = "https://www.reddit.com/api/v1/access_token";
-        var xhr = new XMLHttpRequest();
-        let that = this;
-        xhr.onreadystatechange = function () {
-          var DONE = 4; // readyState 4 means the request is done.
-          var OK = 200; // status 200 is a successful return.
-          if (xhr.readyState === DONE) {
-            if (xhr.status === OK) {
-              let data = JSON.parse(xhr.responseText);
-              console.log("DATA", data);
-              localStorage.setItem("reddit_access_token", data.access_token);
-              // window.history.pushState("", "Greendit", "http://localhost:4200");
-            }
-          }
-        }
-        xhr.open('POST', postLink);
-        xhr.setRequestHeader('Authorization', this.reddit.getBasicAuth());
-        xhr.setRequestHeader('Content-type', "application/x-www-form-urlencoded");
-        xhr.send("grant_type=refresh_token&refresh_token="+localStorage.getItem("reddit_refresh_token"));
+
       }
       return of(err.message);
     }

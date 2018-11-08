6eba56c47478e1060bccd744a1e69a9fb8265b11
Marcin Parafiniuk <marcin.parafiniuk@adm.uw.edu.pl>
2018-11-08 15:11:09 +0100

    <deploy>

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ sed -i "2s/./${newbuildnum}/g" buildnum.ts
 ng build --prod --base-href "/greendit/" --deploy-url "/greendit/"
 rm -rf docs
 mv dist docs
+mv docs/greendit/* docs
+rm -rf docs/greendit
 
 git add --all
 git commit -m"<deploy>"

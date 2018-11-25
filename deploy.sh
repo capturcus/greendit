e9246bfec1c3f21500d4a5939174db691b8ac655
Marcin Parafiniuk <mp347235@students.mimuw.edu.pl>
2018-11-25 15:23:09 +0100

    <deploy>

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 # increment build number
 buildnum=`cat buildnum.ts|head -2|tail -1`
 newbuildnum=$((buildnum+1))
-sed -i "2s/./${newbuildnum}/g" buildnum.ts
+sed -i "2s/.*/${newbuildnum}/g" buildnum.ts
 
 ng build --prod --base-href "/greendit/" --deploy-url "/greendit/"
 rm -rf docs
